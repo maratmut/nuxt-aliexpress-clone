15d5ffadba4f711d2d5af1b442e12cc563545f8b
Marat Mutalipov <maratios41@gmail.com>
2023-06-11 21:49:15 +0300

    Setup project. Create MainLayout component. Setup pinia store

---
+++ components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="FooterMain" class="w-full bg-[#E8E8E8] mt-10 mb-2">
+    <div class="max-w-[1200px] mx-auto px-4">
+
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-sm font-semibold text-[#282828]">Помощь</div>
+          <div class="text-[#999999] text-sm">
+            Помощь центра, Споры и Жалобы, Защита покупателей
+          </div>
+        </div>
+        <div class="md:w-[50%] pt-4">
+          <div class="text-sm font-semibold text-[#282828]">AliExpress Multi-language sites</div>
+          <div class="text-[#999999] text-sm">
+            Russian, Portuguese, Spanish, Italian, Dutch, Turkish...
+          </div>
+        </div>
+      </div>
+      <div class="md:flex gap-4 justify-between w-full">
+        <div class="md:w-[50%] pt-4">
+          <div class="text-sm font-semibold text-[#282828]">Browse by category</div>
+          <div class="text-[#999999] text-sm">All Popular, Product, Promotion</div>
+        </div>
+        <div class="md:w-[50%] pt-4">
+          <div class="text-sm font-semibold text-[#282828]">Alibaba group</div>
+          <div class="text-[#999999] text-sm">Alibaba group website, AliExpress, Alipay</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div id="FooterSub" class="w-full bg-[#333333] text-sm text-gray-400">
+    <div class="max-w-[1200px] mx-auto p-4">
+        Intellectual property protection - Privacy policy - Sitemap - Terms of use.
+    </div>
+  </div>
+</template>
+<script setup></script>
